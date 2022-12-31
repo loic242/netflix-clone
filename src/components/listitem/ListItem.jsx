0fb49e0c9866e44f869239a9bf94899175601909
loic242 <bmavah@gmail.com>
2022-12-31 22:17:58 +0200

    starting with hover effect

--- src/components/listitem/ListItem.jsx
+++ src/components/listitem/ListItem.jsx
@@ -1,9 +1,19 @@
 import "./listitem.scss"
+import movie from '../../assets/movie.jpg';
+import { Add, PlayArrow, ThumbDownOutlined, ThumbUpAltOutlined } from "@material-ui/icons";
 
 function ListItem() {
   return (
     <div className="listItem">
-        List Item
+        <img src={movie} alt="" />
+        <div className="itemInfo">
+          <div className="icons">
+            <PlayArrow/>
+            <Add/>
+            <ThumbUpAltOutlined/>
+            <ThumbDownOutlined/>
+          </div>
+        </div>
     </div>
   )
 }
