2000492e0e30a354932e22cc2286509dd9ff7761
loic242 <bmavah@gmail.com>
2023-01-01 20:47:07 +0200

    lo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import Home from './home/Home';
+import Home from './page/home/Home';
 
 const App = () => {
   return <Home/>
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,7 +1,7 @@
 
-import Featued from "../components/featured/Featued"
-import NavBar from "../components/navbar/NavBar"
-import List from "../components/list/List"
+import Featued from "../../components/featured/Featued"
+import NavBar from "../../components/navbar/NavBar"
+import List from "../../components/list/List"
 import "./home.scss"
 
 const Home = () => {
